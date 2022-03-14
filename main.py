b40b40ab76c1225eb093e2aa392c8b69e6e19e27
Nick-Plis <91833206+Nick-Plis@users.noreply.github.com>
2022-03-14 17:09:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 import pandas as pd
-import pyodbc
 import json
 import os.path
 
-def json_check():
+
+try:
+    df_codewords = pd.read_csv('rb_codewords.csv')
     file = open('file.json')
     data = json.load(file)
     df_main = pd.DataFrame(data)
-    return df_main
-
-
-df_codewords = pd.read_csv('rb_codewords.csv')
-
-while True:
-    try:
-        df_main = json_check()
-        if  df_codewords['codeword'].isin(df_main['codeword']):
-            df_main['request_time'] = pd.to_datetime(df_main['request_time'])
-            df_codewords['date_to'] = pd.to_datetime(df_codewords['date_to'])
-            if df_main['request_time'] < df_codewords['date_to']:
-                pass
-            else:
-                df_main = pd.DataFrame(df_main['id', 'request_ip', 'request_time',
-                                               'district_id', 'where', 'when', 'importance_id',
-                                               'request', 'phone', 'name'], columns=df_main.columns)
+    if df_codewords['codeword'].isin(df_main['codeword']):
+        df_main['request_time'] = pd.to_datetime(df_main['request_time'])
+        df_codewords['date_to'] = pd.to_datetime(df_codewords['date_to'])
+        if df_main['request_time'] < df_codewords['date_to']:
+            pass
         else:
             df_main = pd.DataFrame(df_main['id', 'request_ip', 'request_time',
                                            'district_id', 'where', 'when', 'importance_id',
                                            'request', 'phone', 'name'], columns=df_main.columns)
 
-        df_main.to_csv(r'main_requests.csv', mode='a', header=False)
+    else:
+        df_main = pd.DataFrame(df_main['id', 'request_ip', 'request_time',
+                                       'district_id', 'where', 'when', 'importance_id',
+                                       'request', 'phone', 'name'], columns=df_main.columns)
 
-    except:
-        continue
+except:
+    pass
 
+print('123')
+df_main.to_csv(r'main_requests.csv', header=False, index=False)
